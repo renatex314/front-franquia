803d4a2f1dbb56e69773edcb859b9d162bcafb84
renatex314 <renatocorte34@gmail.com>
2023-11-17 17:47:35 -0300

    [tela informações curso] - 17/11/2023 - Renato Corte

--- src/app/(auth)/student/courses/[matriculaId]/page.tsx
+++ src/app/(auth)/student/courses/[matriculaId]/page.tsx
@@ -23,8 +23,21 @@ const SelectedCoursePage = ({
   }, [router, selectedMatriculaId]);
 
   return (
-    <div className="flex flex-col w-full h-full items-center pt-10">
-      <Card>teste</Card>
+    <div className="flex flex-col w-full h-full items-center pt-10 gap-3">
+      <Card className="flex flex-col w-[50%] gap-1">
+        <div className="flex items-center">
+          <p>Curso de Inglês</p>
+          <p className="mx-2">-</p>
+          <p className="mr-auto">Inglês</p>
+          <div className="w-3 h-3 rounded-full shrink-0 grow-0 bg-green-500"></div>
+          <p className="ml-3">Matrícula ativa</p>
+        </div>
+        <div className="flex text-gray-400 gap-3">
+          <p>iniciante</p>
+          <p className="ml-auto">Matriculado desde 03/11/2022</p>
+        </div>
+      </Card>
+      <Card className="w-[50%]">teste de card</Card>
     </div>
   );
 };
